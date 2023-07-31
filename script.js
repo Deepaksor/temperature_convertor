2594dedfeba990bc7a51f5de4085831ac9fed8fd
Deepaksor <137423091+Deepaksor@users.noreply.github.com>
2023-07-31 20:36:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const celsiusInput = document.getElementById('celsius');
+const fahrenheitInput = document.getElementById('fahrenheit');
+const convertBtn = document.getElementById('convertBtn');
+
+document.addEventListener('DOMContentLoaded', function() {
+    convertBtn.addEventListener('click', function() {
+        if (celsiusInput.value !== '') {
+            const celsius = parseFloat(celsiusInput.value);
+            const fahrenheit = (celsius * 9/5) + 32;
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+        } else if (fahrenheitInput.value !== '') {
+            const fahrenheit = parseFloat(fahrenheitInput.value);
+            const celsius = (fahrenheit - 32) * 5/9;
+            celsiusInput.value = celsius.toFixed(2);
+        }
+    });
+});
